ea86f85c63730788b9446150d28dea61d32a7c08
Lucas-Haux <hauxlucas2005@gmail.com>
2024-08-23 17:19:50 -0700

    Feature: search nix packages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use clap::Parser;
+use reqwest::Client;
+use serde_json::Value;
+use serde_json::json;
+use std::error::Error;
+use std::fs;
+use std::io;
  
 #[derive(Parser)]
 #[clap(author, version)]
@@ -7,18 +13,56 @@ struct Args {
     wolfpack: String,
 
     #[clap(value_parser)]
-    first: f32,
+    query: String,
 
-    #[clap(value_parser)]
-    second: f32
 }
 
 fn main() {
     let args = Args::parse();
 
     let result = match args.wolfpack.as_str() {
-        "+" => args.first + args.second,
+        "s" => query_search(args.query),
         _ => panic!("idk")
     };
-    println!("{}", result);
 }
+
+#[tokio::main]
+async fn query_search(search: String) -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+    let url = "https://search.nixos.org/backend/latest-42-nixos-24.05/_search";
+    let json_data = fs::read_to_string("query.json")?;
+    let mut query_body: Value = serde_json::from_str(&json_data)?;
+
+    // Update the field 
+    query_body["query"]["bool"]["must"][0]["dis_max"]["queries"][0]["multi_match"]["query"] = json!(search);
+    fs::write("query.json", serde_json::to_string_pretty(&query_body)?)?;
+
+    // Make the request with API key included in the headers
+    let response = client
+        .post(url)
+        .header("Authorization", "Bearer YVdWU0FMWHBadjpYOGdQSG56TDUyd0ZFZWt1eHNmUTljU2g=")
+        .json(&query_body)
+        .send()
+        .await?;
+    
+    // Get the raw JSON response as a Value
+    let response_json: Value = response.json().await?;
+    
+    // Extract and print `package_pname` from the response
+    if let Some(hits) = response_json["hits"]["hits"].as_array() {
+        for hit in hits {
+            if let Some(_score) = hit.get("_score") {
+                println!("Package score: {}", _score);
+            }
+            match hit["_source"].get("package_attr_name") {
+                Some(package_attr_name) => println!("Package Name: {}", package_attr_name),
+                None => continue,
+            }
+        }
+    } else {
+            eprintln!("Unexpected response format.");
+    };
+    
+    Ok(())
+}
+
